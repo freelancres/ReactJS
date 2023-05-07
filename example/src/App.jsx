012e4f937b98610aaf71138fcc25d1f7f313a408
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2023-05-07 16:39:06 +0200

    Add files via upload

---
+++ example/src/App.jsx
@@ -0,0 +1,17 @@
+import Lulu from './components/Header/Header.jsx';
+import { Main } from './components/Main/Main.jsx';
+import { Footer as Susu } from "./components/Footer/Footer";
+ 
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Lulu />
+    <Main />
+    <Susu />
+  </div>
+);
+
+
+
+export default App;
---
+++ example/src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import "./Main.css";
+
+function Main  ()  {
+  return (
+    <main className="main">
+      <h2>Main Content</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </main>
+  );
+}
+
+export { Main };
